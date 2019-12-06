c013f7a2e6206884d0dc0e054682262da32e5deb
fonkamloic <fonkamloic@gmail.com>
2019-12-06 18:57:21 +0100

    main file

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:pl_learning/screens/LoadingScreen.dart';
+import 'package:pl_learning/screens/LoginScreen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoadingScreen(),
+    );
+  }
+}
